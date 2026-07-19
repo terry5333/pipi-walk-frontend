106eb54524d947d9f942a8ec5e19a7454684f630
Terry <terrylie0215@gmail.com>
2026-07-19 20:28:50 +0800

    Update sw.js to fix cache and trailing slashes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pipi-walk-v1';
+const CACHE_NAME = 'pipi-walk-v2';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
@@ -10,11 +10,27 @@ const ASSETS_TO_CACHE = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
